9aaf7587bf1184829a2443de45de3d53da593446
Liam Zhang <liamzhang@microsoft.com>
2023-04-14 16:14:56 -0700

    added react-intl

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { IntlProvider } from 'react-intl'
+import messages from './compiled-lang/en-US.json'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <IntlProvider locale='en-US' defaultLocale='en-US' messages={messages}>
+      <App />
+    </IntlProvider>
   </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: { port: 3000 },
+  plugins: [
+    react({
+      babel: {
+        "plugins": [
+          [
+            "formatjs",
+            {
+              "idInterpolationPattern": "[sha512:contenthash:base64:6]",
+              "ast": true
+            }
+          ]
+        ]
+      }
+    } )
+  ],
 })
